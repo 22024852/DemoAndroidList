8e60f30cee23fd5f3072249f681ed82bc490b5be
22024852 <22024852@myrp.edu.sg>
2023-07-21 15:58:35 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22024852/demoandroidlist/MainActivity.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.c346.id22024852.demoandroidlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayList<AndroidVersion> al;
+    //ArrayAdapter<String> aa;
+    CustomAdapter aa;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = findViewById(R.id.listView);
+        al = new ArrayList<>();
+
+        AndroidVersion item1 = new AndroidVersion("Pie", "9.0");
+        AndroidVersion item2 = new AndroidVersion("Oreo", "8.0-8.1");
+        AndroidVersion item3 = new AndroidVersion("Nougat", "7.0 -7.1.2");
+        al.add(item1);
+        al.add(item2);
+        al.add(item3);
+
+
+        // aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,al);
+        aa = new CustomAdapter(this, R.layout.row, al);
+        lv.setAdapter(aa);
+
+
+    }
+}
